28e34d3e54e1e039a4c2cbc1f03e9de27455270a
Kaiter Wu <wukaiter@gmail.com>
2023-03-09 17:10:45 -0500

    testing test

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,7 +1,7 @@
 class Example {
     constructor(ele) {
         this.ele = ele
-        this.ele.innerHTML = "<h1>It's bCAPYS ALIVEEEE!!!</h1>"
+        this.ele.innerHTML = "<h1>It's DANCAPYS ALIVEEEE!!!</h1>"
 
         this.handleClick = this.handleClick.bind(this);
         this.ele.addEventListener("click", this.handleClick)
