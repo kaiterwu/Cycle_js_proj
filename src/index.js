57a11ded4d0545d27a39a42713435d74be0467ed
Kaiter Wu <wukaiter@gmail.com>
2023-03-09 16:47:52 -0500

    going through walkthrough

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Example from "./scripts/example"
+
+document.addEventListener("DOMContentLoaded", () => {
+    // console.log("Hello world!")
+    const main = document.getElementById("main")
+    new Example(main);
+
+});
+
