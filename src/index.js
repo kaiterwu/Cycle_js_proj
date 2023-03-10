c31f9a6dca3448b3698584b91011b759b0d6350f
Kaiter Wu <wukaiter@gmail.com>
2023-03-10 10:34:45 -0500

    testing modals and other functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import Example from "./scripts/example"
-import getCurrentData from "./scripts/data";
 
 document.addEventListener("DOMContentLoaded", () => {
     // console.log("Hello world!")
     const main = document.getElementById("main")
     new Example(main);
-    const currentData = getCurrentData()
-    window.currentData = currentData
+    
+    const apiKey = "4P48W2KCWFQAQ6SJ99K5H2VBM"
+    const zip = "11040"
+    const now = Math.floor(Date.now()/1000)
+
+
+    async function getCurrentData() {
+    
+    try {
+       const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${zip}/${now}?key=${apiKey}&include=current`);
+       if (response.ok) {
+         const data = await response.json();
+
+         console.log(data);
+       } else {
+         throw response;
+       }
+    } catch (errorResponse) {
+        console.error(errorResponse);
+    }
+  }
+
+  getCurrentData()
 
 });
 
