54706fceb501ccbc305b80f2ef47c46a9912e429
Kaiter Wu <wukaiter@gmail.com>
2023-03-10 22:53:56 -0500

    node

--- src/scripts/sunmoves.js
+++ src/scripts/sunmoves.js
@@ -7,36 +7,45 @@ export function sunPath(data){
     const homeData = document.getElementsByClassName("astro-data")[0]
     homeData.append(sunrise)
     homeData.append(sunset)
-}
+};
 
     //set the dimensions and margins of the graph
-export function drawPath(){
+export function drawPath(queryData){
 
-    const width = 450,
-        height = 450,
-        margin = 40;
+    const width = 250,
+        height = 250,
+        margin = 20;
 
-    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    // The radius of the pieplot is half the width or half the height (smallest one). Margin slightly subtracted 
     const radius = Math.min(width, height) / 2 - margin
 
-    // append the svg object to the div called 'my_dataviz'
-    const svg = d3.select("sun-path")
+    // append the svg object to the div called 'sun-path'
+    const svg = d3.select("#sun-path")
     .append("svg")
         .attr("width", width)
         .attr("height", height)
     .append("g")
         .attr("transform", `translate(${width / 2},${height / 2})`);
+    
+    //dynamic querydata 
+        
 
-    // Create dummy data
-    const data = {a:2000,b:2000}
+    // data object
+    const data = {a:2000,b:2000,c:2000}
 
-    // set the color scale
+    // set color
     const color = d3.scaleOrdinal()
-    .range(["#ffff", "#7f1ea2",])
+    .range(["#ffffff","#ebb811", "#7f1ea2",])
 
     // Compute the position of each group on the pie:
     const pie = d3.pie()
-    .value(d=>d[1]).sort(null)
+    .value(d=>d[1])
+    .sort(null)
+
+    const arc =  d3.arc()
+    .innerRadius(80)
+    .outerRadius(radius);
+    
         
 
 
@@ -44,17 +53,37 @@ export function drawPath(){
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
     svg
-    .selectAll('whatever')
+    .selectAll('path')
     .data(data_ready)
     .join('path')
-    .attr('d', d3.arc()
-        .innerRadius(100)         // This is the size of the donut hole
-        .outerRadius(radius)
-    )
-    .attr('fill', d => color(d.data[0]))
+    // .attr('d', d3.arc()
+    //     .innerRadius(60)         // Inner circle hole size 
+    //     .outerRadius(radius))
+    .transition().delay(function(d, i) { return i * 400; })
+    .duration(400)
+    .attrTween('d', function(d) {
+        let i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+        return function(t) {
+            d.endAngle = i(t);
+          return arc(d);
+        };
+   })
+    .style('fill', d => color(d.data[0]))
     .attr("stroke", "white")
     .style("stroke-width", "5px")
     .style("opacity", 1)
+    
+
+    svg.append("path")
+    .style("fill", function(d) { return color(d.data.name); })
+    .transition().delay(function(d, i) { return i * 500; }).duration(500)
+    .attrTween('d', function(d) {
+       let i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+       return function(t) {
+           d.endAngle = i(t);
+         return d3.svg.arc(d);
+       };
+  });
 }
 
 
