1838d520ff113d8be26c9f734a3347bc1fdc565a
Kaiter Wu <wukaiter@gmail.com>
2023-05-08 19:58:59 -0400

    removing unused code from proj

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-
-import {drawSun,openSunModal}  from "./scripts/sunmoves";
-import { drawSeasons,openSeasonsModal } from "./scripts/seasons";
-import { drawMoon,openMoonModal } from "./scripts/moonPhase";
-import { fetchData,getData,getClock, getCurrentData } from "./scripts/data";
-import { getDate,callclock } from "./scripts/timeConvert";
 import { openIntroModal,volume } from "./scripts/intro_modal";
 import { openSelectModal } from "./scripts/select_modal";
 
